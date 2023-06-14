2b5ca3052560163e928f31330542b93f494158f5
MartinL-no <martin.lorimer@icloud.com>
2023-06-14 21:33:07 +0200

    Add project files.

---
+++ NewsLetterExercise.Core/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NewsLetterExercise.Core
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
