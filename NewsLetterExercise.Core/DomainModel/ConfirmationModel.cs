83ca5b336de42bbe7e922d2e69380f5bcbd7cb9a
MartinL-no <martin.lorimer@icloud.com>
2023-06-15 13:35:59 +0200

    feat: create ConfirmationModel

---
+++ NewsLetterExercise.Core/DomainModel/ConfirmationModel.cs
@@ -0,0 +1,16 @@
+﻿namespace NewsLetterExercise.Core.DomainModel
+{
+    public class ConfirmationModel
+    {
+        public readonly string Name;
+        public readonly string Email;
+        public readonly Guid ConfirmationCode;
+
+        public ConfirmationModel(string name, string email, Guid confirmationCode)
+        {
+            Name = name;
+            Email = email;
+            ConfirmationCode = confirmationCode;
+        }
+    }
+}
